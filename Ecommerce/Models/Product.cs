c410c1b635555afaaac19cbd83144c62c86fec00
DouglasHutchful1 <douglas46ibk@gmail.com>
2025-12-22 18:31:23 -0800

    Add data annotations to Product model for improved validation and database mapping

--- Ecommerce/Models/Product.cs
+++ Ecommerce/Models/Product.cs
@@ -1,18 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Ecommerce.Models
 {
+    [Table("Product")]
     public class Product
     {
+        [Key]
+        
         public int Id { get; set; }
         public string Name { get; set; } 
         public string? Category { get; set; }
         
         public decimal Price { get; set; }
+        public int? Stock { get; set; }
         public string? ImageUrl { get; set; }
         public bool Active { get; set; }
         public DateTime CreationDate { get; set; }
         public int CreatedBy { get; set; } 
-        public DateTime EditedDate { get; set; }
-        public int EditedBy { get; set; }
+        public DateTime? EditedDate { get; set; }
+        public int? EditedBy { get; set; }
         
         
     }
